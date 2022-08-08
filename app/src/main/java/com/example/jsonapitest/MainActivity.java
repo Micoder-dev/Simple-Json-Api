fc1f66028f80f5e805843e00b648b50be4d2f10b
Micoder-dev <micoder.com@gmail.com>
2022-08-08 15:29:45 +0530

    retrieved strings are displayed in textView & imageView

--- app/src/main/java/com/example/jsonapitest/MainActivity.java
+++ app/src/main/java/com/example/jsonapitest/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -12,6 +14,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,11 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
     RequestQueue mRequestQueue;
 
+    private TextView myTextView;
+    private ImageView myImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myTextView = findViewById(R.id.myText);
+        myImageView = findViewById(R.id.myImage);
 
         mRequestQueue = Volley.newRequestQueue(this);
 
@@ -41,9 +49,12 @@ public class MainActivity extends AppCompatActivity {
 
                             String mytext = response.getString("mytext");
                             Toast.makeText(MainActivity.this, mytext + "", Toast.LENGTH_LONG).show();
+                            myTextView.setText(mytext);
 
                             String myimage = response.getString("myimage");
                             Toast.makeText(MainActivity.this, myimage + "", Toast.LENGTH_LONG).show();
+
+                            Picasso.get().load(myimage).placeholder(R.drawable.ic_launcher_background).into(myImageView);
 
                         } catch (JSONException e) {
                             Toast.makeText(MainActivity.this, "Something went wrong, please check your internet connection", Toast.LENGTH_SHORT).show();
